80d500685648373248e4dfc9e8b9e9d79908053e
zaid03 <terguyzaid@gmail.com>
2025-10-31 13:48:14 +0100

    -removed unecessary files for bolsas

--- backend/src/main/java/com/example/backend/sqlserver2/repository/GbsRepository.java
+++ backend/src/main/java/com/example/backend/sqlserver2/repository/GbsRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.example.backend.sqlserver2.repository.GbsRepository;
 import com.example.backend.sqlserver2.model.Gbs;
 import com.example.backend.sqlserver2.model.GbsId;
 
